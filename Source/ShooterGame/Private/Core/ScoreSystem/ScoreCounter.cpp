012648b3d8d7abbcc5c27c9cb23d042e7aaa27d5
senseImamuraY <89624108+senseImamuraY@users.noreply.github.com>
2023-11-06 12:57:15 +0900

    fixed bug
    Pickup WidgetにUICollision判定があり、トレースに失敗していたので修正しました。

---
+++ Source/ShooterGame/Private/Core/ScoreSystem/ScoreCounter.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/ScoreSystem/ScoreCounter.h"
+
+UScoreCounter::UScoreCounter(const FObjectInitializer& ObjectInitializer) :
+	UUserWidget(ObjectInitializer),
+	ScoreText(nullptr),
+	TotalScore(0)
+{
+}
+
+void UScoreCounter::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	UTextBlock* pTextBlock = Cast<UTextBlock>(GetWidgetFromName("ScoreText"));
+	ensure(pTextBlock != nullptr);
+	ScoreText = pTextBlock;
+	
+	//if (pTextBlock)
+	//{
+	//	ScoreText = pTextBlock;
+	//	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("ScoreText TextBlock found."));
+	//}
+	//else
+	//{
+	//	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("ScoreText TextBlock not found."));
+	//}
+
+	UpdateScore(0);
+}
+
+
+void UScoreCounter::UpdateScore(int score)
+{
+	TotalScore += score;
+	FString str = FString::FromInt(TotalScore);
+	ensure(ScoreText != nullptr);
+
+	if (ScoreText)
+	{
+		ScoreText->SetText(FText::FromString(str));
+	}
+}
+
--- Source/ShooterGame/Private/Player/ShooterCharacter.cpp
+++ Source/ShooterGame/Private/Player/ShooterCharacter.cpp
@@ -24,6 +24,10 @@
 #include "../Public/Core/LevelSystem/PlayerLevelSystem.h"
 #include "../Public/Core/InGameHUD.h"
 
+
+#include "NiagaraSystem.h"
+#include "NiagaraComponent.h"
+
 // Sets default values
 AShooterCharacter::AShooterCharacter() : 
 	// 基本のrate
@@ -446,6 +450,46 @@ bool AShooterCharacter::TraceUnderCrosshairs(FHitResult& OutHitResult, FVector& 
 		if (OutHitResult.bBlockingHit)
 		{
 			OutHitLocation = OutHitResult.Location;
+
+			if (OutHitResult.bBlockingHit)
+			{
+				OutHitLocation = OutHitResult.Location;
+
+				// ヒットしたアクターの名前を取得
+				AActor* HitActor = OutHitResult.GetActor();
+				if (HitActor)
+				{
+					FString ActorName = HitActor->GetName();
+
+					// GEngineを使用して画面にアクターの名前を出力
+					if (GEngine)
+					{
+						GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("Hit Actor: %s"), *ActorName));
+					}
+					// Check if the hit was on a CollisionBox
+					if (OutHitResult.Component->IsA<UBoxComponent>())
+					{
+						GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Hit a CollisionBox"));
+					}
+					// ヒットしたコンポーネントがNiagaraパーティクルシステムのものかどうかをチェック
+					else if (OutHitResult.Component->IsA<UNiagaraComponent>())
+					{
+						// Niagaraパーティクルシステムにヒットしたことをログに出力
+						if (GEngine)
+						{
+							GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Purple, TEXT("Hit a Niagara Particle System"));
+						}
+					}
+					// Check if the hit was on a mesh
+					else if (OutHitResult.Component->IsA<UMeshComponent>())
+					{
+						GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Hit a Mesh"));
+					}
+				}
+
+				return true;
+			}
+
 			return true;
 		}
 	}
