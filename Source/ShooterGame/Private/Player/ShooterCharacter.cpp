3717284a52ebcc349c84aa52766159ff81c06888
senseImamuraY <89624108+senseImamuraY@users.noreply.github.com>
2023-12-30 21:47:13 +0900

    deleted BP_BaseWeapon
    ・BP_BaseWeaponを削除
    ・リダイレクターを変更

--- Source/ShooterGame/Private/Player/ShooterCharacter.cpp
+++ Source/ShooterGame/Private/Player/ShooterCharacter.cpp
@@ -662,7 +662,7 @@ void AShooterCharacter::GrabClip()
 	ClipTransform = EquippedWeapon->GetItemMesh()->GetBoneTransform(ClipBoneIndex);
 
 	FAttachmentTransformRules AttachmentRules(EAttachmentRule::KeepRelative, true);
-	HandSceneComponent->AttachToComponent(GetMesh(), AttachmentRules, FName(TEXT("Hand_L")));
+	HandSceneComponent->AttachToComponent(GetMesh(), AttachmentRules, FName(TEXT("hand_l")));
 	HandSceneComponent->SetWorldTransform(ClipTransform);
 
 	EquippedWeapon->SetMovingClip(true);
